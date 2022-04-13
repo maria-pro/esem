564471686363dcef1aabe31926b13bd6a640f5a2
Maria Prokofieva <maria.prokofieva@gmail.com>
2022-04-13 22:04:07 +1000

    first function

--- R/esem.R
+++ R/esem.R
@@ -1,7 +1,13 @@
+#' esem
+#' completes esem estimation using the provided parameters
+#' @param animal is the parameter to control
+#' @param sound is the parameter to control
+#'
+#' @return prints the string
+#' @export
+#'
+#' @examples esem(awesome, beautiful)
 esem <- function(animal, sound) {
-  assertthat::assert_that(
-    assertthat::is.string(animal),
-    assertthat::is.string(sound)
-  )
+  psych::bfi
   paste0("The ", animal, " goes ", sound, "!")
 }
