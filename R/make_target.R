0b21b73f8a0aa5a6320f8829276cb625d028e70b
Maria Prokofieva <maria.prokofieva@gmail.com>
2022-06-27 21:50:11 +1000

    up

--- R/make_target.R
+++ R/make_target.R
@@ -1,6 +1,15 @@
 #Make a target matrix to be used for target rotation in the EFA stage of ESEM
 #It is a wrapper function around make.keys() function from the psych package
 
+#' Title
+#'
+#' @param data is a dataset to be used in EFA
+#' @param keys is a key matrix with the primary factor items. It can be made with the make.keys() function.
+#' The primary factor items in the matrix are  used as referent items.
+#' @return a list with target matrix
+#' @export
+#'
+#' @examples
 make_target<-function(data, keys){
   if (is.null(data)) {
     rlang::abort("bad argument", message = "Please provide the dataset")
